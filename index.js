66a56f48589eb3e8f74b4f7eb5435400283b0149
João Manuel <johnjohn200513@gmail.com>
2025-11-13 16:18:03 -0300

    ✨feat(cors): adciona cors do frontend

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors';
 import routes from "./routes/indexRoutes.js"
 import dotenv from "dotenv";
 import db from "./config/dbConfig.js"
@@ -7,6 +8,11 @@ dotenv.config();
 await db.sync({ alter: true });
 
 const app = express();
+
+app.use(cors({
+  origin: 'http://127.0.0.1:5500' //colocar seu ip e porta que esta rodando o frontend
+}));
+
 routes(app);
 
 const PORT = process.env.PORT || 3000;
