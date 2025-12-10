dbf7bea792b9317e66b8cdc2e26ca236fd53497a
João Manuel <johnjohn200513@gmail.com>
2025-12-10 14:57:47 -0300

    ✨feat(usuario): adciona rotas do usuario

--- routes/indexRoutes.js
+++ routes/indexRoutes.js
@@ -1,9 +1,13 @@
 import express from "express";
 
 import evento from "./eventoRouter.js"
+import auth from "./authRouter.js"
+import usuario from "./usuarioRouter.js"
 
 const routes = (app) => {
      app.use(express.json(), evento);
+     app.use(express.json(), auth)
+     app.use(express.json(), usuario)
 }
 
 export default routes;
